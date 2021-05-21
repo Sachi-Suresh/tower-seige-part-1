e85494f97ae06e784e9502e5c09a7c06645a3b87
Sachi-Suresh <77390698+Sachi-Suresh@users.noreply.github.com>
2021-05-21 17:51:40 +0530

    Update slingshot.js

--- slingshot.js
+++ slingshot.js
@@ -1,25 +1,33 @@
-class slingshot{
-    constructor(bodyA,pointB){
-        var options= {
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
             bodyA: bodyA,
             pointB: pointB,
             stiffness: 0.04,
             length: 1
         }
-        this.sling= Constraint.create(options);
-        this.pointB= pointB;
-        World.add(world,this.sling);
+        this.sling = Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world, this.sling);
+
     }
+    //write code for attach( ) here
+attach(body){
+    this.sling.bodyA= body;
+}
     fly(){
-        this.sling.bodyA= null;
+        this.sling.bodyA =null;
+        
     }
     display(){
         if(this.sling.bodyA){
-            var pointA= this.sling.bodyA.position;
-            var pointB= this.pointB;
-            strokeWeight(4);
-            stroke("turquoise");
-            line (pointA.x,pointA.y,pointB.x,pointB.y);
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        
+        strokeWeight(4);
+        stroke("turquoise");
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
         }
-    }
+        }
+    
 }
